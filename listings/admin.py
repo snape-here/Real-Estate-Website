dd0b061a79697669fb4b42399591f68b2e1bdd33
Susanta Mukherjee <susantamukherjee392@gmail.com>
2021-05-26 20:02:16 +0530

    Added Admin area and listings

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+        list_display = ('id', 'title', 'is_published', 'price', 'realtor')
+        list_display_links = ('id', 'title')
+        list_filter = ('realtor',)
+        list_editable = ('is_published',)
+        search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+        sortable_by = ('price', 'bedrooms', 'sqft', 'lot_size')
+        list_per_page = 20
 
 # Register your models here.
+admin.site.register(Listing, ListingAdmin)
