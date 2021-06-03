849654123a27949c68a8a89c3d7a6883d037ebaf
Susanta Mukherjee <susantamukherjee392@gmail.com>
2021-06-03 14:14:50 +0530

    Updated Settings

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['realestateco.herokuapp.com', 'realestateco.netlify.app']
+ALLOWED_HOSTS = ['realestateco.herokuapp.com',]
 
-
+ 
 # Application definition
 
 INSTALLED_APPS = [
