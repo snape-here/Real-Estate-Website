304bd67ff11e94fdc9416d8a2163cb8e0b180e16
Susanta Mukherjee <susantamukherjee392@gmail.com>
2021-06-03 13:37:53 +0530

    Added Netlify to hosts allowed

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['realestateco.herokuapp.com']
+ALLOWED_HOSTS = ['realestateco.herokuapp.com', 'realestateco.netlify.app']
 
 
 # Application definition
